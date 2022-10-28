c14193de7c46bde8331637aa84daaf6303a70723
nicola ruggiero <nicoruggi97@live.it>
2022-10-28 14:48:48 +0200

    Added V3 network for images descriptors

--- main.py
+++ main.py
@@ -7,12 +7,13 @@ import imutils
 import cv2
 import os
 import glob
+from PIL import Image
 
-"""
 import tensorflow as tf
 import tensorflow_hub as hub
 
-def V3LargePredict(img, IMAGE_SHAPE):
+
+def V3LargePredict(img, IMAGE_SHAPE=(224, 224)):
     model = tf.keras.Sequential([
         hub.KerasLayer("https://tfhub.dev/google/imagenet/mobilenet_v3_large_075_224/classification/5",
                        trainable=True, arguments=dict(batch_norm_momentum=0.997))
@@ -23,22 +24,21 @@ def V3LargePredict(img, IMAGE_SHAPE):
 
     result = model.predict(img[np.newaxis, ...])
 
-    print('result :' + str(result))
-    print('modulo :' + str(np.linalg.norm(result)))
+    # print('result :' + str(result))
+    # print('modulo :' + str(np.linalg.norm(result)))
     return result
-    """
 
 
-def image_to_feature_vector(image, size=(450, 450)):
+def image_to_feature_vector(image):
     # resize the image to a fixed size, then flatten the image into
     # a list of raw pixel intensities
-    return cv2.resize(image, size).flatten()
+    return V3LargePredict(image)
 
 
 def KNN(train_set, train_labels, test_set, test_true_labels, k):
-    classifier = KNeighborsClassifier(n_neighbors=k)
-    classifier.fit(train, train_labels)
-    results = classifier.score(test, test_true_labels)
+    classifier = KNeighborsClassifier(n_neighbors=k, )
+    classifier.fit(train_set, train_labels)
+    results = classifier.score(test_set, test_true_labels)
     print("mean accuracy :" + str(results) + ' con k = ' + str(k))
 
 
@@ -49,7 +49,7 @@ if __name__ == '__main__':
     test_true_labels = []
     k_values = [2, 3, 5, 7, 10]
     for filename in glob.glob('Test_images/' + '*jpg'):
-        img = cv2.imread(filename)
+        img = Image.open(filename)
         img = image_to_feature_vector(img)
         test.append(img)
         if 'autore' in filename:
@@ -61,17 +61,20 @@ if __name__ == '__main__':
         else:
             test_true_labels.append(0)
 
+    print('Done Test')
+    """
     for filename in glob.glob('train_images/partir/' + '*jpg'):
-        img = cv2.imread(filename)
+        img = Image.open(filename)
         img = image_to_feature_vector(img)
         train.append(img)
         if 'autore' in filename:
             train_labels.append(1)
         elif 'partir' in filename:
             train_labels.append(2)
-
+    print('Done Train,partir')
+    """
     for filename in glob.glob('train_images/calco/' + '*jpg'):
-        img = cv2.imread(filename)
+        img = Image.open(filename)
         img = image_to_feature_vector(img)
         train.append(img)
         if 'autore' in filename:
@@ -79,7 +82,7 @@ if __name__ == '__main__':
         elif 'partir' in filename:
             train_labels.append(2)
 
-    print("len di test :" + str(len(test)))
+    print('Done Train,calco')
 
     for i in range(0, len(k_values)):
         KNN(train, train_labels, test, test_true_labels, k_values[i])
